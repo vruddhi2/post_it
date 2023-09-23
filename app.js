91a9e372684c7331a51dfbfcef7b7fb885e31b5b
Coldcoffee303 <sharathkodidala7@gmail.com>
2023-09-23 21:47:54 +0530

    solve conflict with login, register

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const indexRouter = require('./project-server/routes/index');
 
+const indexRouter = require('./project-server/routes/index');
 const userRouter = require('./project-server/routes/users');
 const {requireAuth} = require('./middleware/auth');
 
@@ -16,10 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'project-server/public')));
 
 app.set('views', path.join(__dirname, 'project-server', 'views'))
-app.set('view engine', 'pug');
-app.use('/', requireAuth , indexRouter);
+app.set('view engine', 'ejs');
+
+
+app.use('/', requireAuth, indexRouter);
 app.use('/user', userRouter);
+
 module.exports = app;
--- project-server/routes/index.js
+++ project-server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Home Page</h1>');
+  res.render('index');
   // res.sendFile('login.html', { root: 'project-server/views' });
 });
 
